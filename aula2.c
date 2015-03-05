cac083ec8b38ab609055c636fd6c295dedaa070b
leosteil <lsteil@inf.ufsm.br>
2015-03-05 11:55:56 -0300

    aula 2

---
+++ aula2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main (){
+	
+	FILE* f;
+	int matricula;
+	char nome [100];
+	printf ("Digite a matricula\n");	
+	scanf ("%d", &matricula);
+	printf ("Digite o nome\n");
+	scanf ("%s", nome);
+	f = fopen("saida.txt", "r");
+	fscanf(f, "%d %s\n", matricula, nome); 
+	fclose(f);
+
+}
