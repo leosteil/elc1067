5da00187f42d8dae2011ab583378bad5786bf004
leosteil <lsteil@inf.ufsm.br>
2015-03-05 11:42:34 -0300

    adicionei cod

--- aula.c
+++ aula.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main (void){
-
-	printf ("Hello World!!\n");
-	printf ("Hi World!!\n");
+void main (){
+	
+	FILE* f;
+	int matricula;
+	char nome [100];
+	printf ("Digite a matricula\n");	
+	scanf ("%d", &matricula);
+	printf ("Digite o nome\n");
+	scanf ("%s", nome);
+	f = fopen("saida.txt", "w");
+	fprintf(f, "%d %s\n", matricula, nome); 
+	fclose(f);
 
 }
