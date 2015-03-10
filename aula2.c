f4a5620607aaefdaabaa04c7e0d416ece07e2c38
leosteil <lsteil@inf.ufsm.br>
2015-03-10 10:40:57 -0300

    arquivo adicionado

--- aula2.c
+++ aula2.c
@@ -3,14 +3,20 @@
 void main (){
 	
 	FILE* f;
-	int matricula;
+	/*int matricula;
 	char nome [100];
 	printf ("Digite a matricula\n");	
 	scanf ("%d", &matricula);
 	printf ("Digite o nome\n");
-	scanf ("%s", nome);
+	scanf ("%s", nome);*/
+	char c[100];
 	f = fopen("saida.txt", "r");
-	fscanf(f, "%d %s\n", matricula, nome); 
+
+	while(fgets(c, 100, f) != NULL){ /*foi a unica forma que consegui fazer, nao consegui fazer usando o fscanf*/
+		printf ("%s", c);
+	}
+
+	//fscanf(f,"%s\n",c); 
 	fclose(f);
 
 }
