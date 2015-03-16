967db86be6b45739c7ae05ab370fa5f46f4ae899
leosteil <lsteil@inf.ufsm.br>
2015-03-15 21:34:44 -0300

    modifiquei função 1 e add função 2

--- T1/t1.c
+++ T1/t1.c
@@ -1,46 +1,82 @@
 #include <stdio.h>
+#include <string.h>
 
-void ler_alunos(int * matriculas, char ** nome, int * n) {
+void ler_alunos(int * matriculas, char nomes[][50], int * n) {
 	int matricula, i = 0, j = 0;
 	char caracter, nome[50];
 
-	FILE * resource = fopen("alunos.txt", "r");
+	FILE *arq;
+	arq = fopen("alunos.txt", "r");
 
-	if (resource == NULL) {
+	if (arq == NULL) {
 		printf("Erro ao abrir um dos arquivos."); return;
 	}
 
-	while (feof(resource) != 0) {
-		if (fscanf(resource, "%d", &matricula)) {
+	while (feof(arq) == 0) {
+		if (fscanf(arq, "%d", &matricula)<=0) {
 			break;
 		}
 
-		caracter = fgetc(resource);
+		caracter = fgetc(arq);
 
-		while (caracter != "\n") {
+		while (caracter != '\n') {
 			nome[i] = caracter;
-			caracter = fgetc(resource);
+			caracter = fgetc(arq);
 			/**/ i++;
 		}
 
-		nome[i] = "\n";
+		nome[i] = '\0';
 		matriculas[j] = matricula;
 		strcpy(nomes[j], nome);
 		/**/ j++;
 	}
 
-	fclose(resource);
-	n = linha;
+	fclose(arq);
+	*n = j;
+}
+
+float media(int * matricula) {
+	int media;
+	float n1, n2;
+
+	FILE *arq;
+	arq = fopen("notas.txt", "r");
+
+	if (arq == NULL) {
+		printf("Erro ao abrir um dos arquivos."); return;
+	}
+
+	while (feof(arq) != 0) {
+		if (fscanf(arq, "%d", &media)) {
+			break;
+		}
+
+		if (media == *matricula) {
+			if (fscanf(arq, "%f %f", &n1, &n2)) {
+				return (n1 + n2) / 2;
+			}
+		}
+	}
+
+	return 0.0;
 }
 
 void main (int argc, char ** argv) {
 
-	int matriculas[50], i;
-	char nomes[50][64], caracter;
-	float n1, n2, media;
+	int matriculas[50], matricula, linhas, linha, i;
+	char nomes[50][64], nome[64], caracter;
 
 	if (argc > 1) {
 		printf("Você deve definir um nome de aluno como parâmetro."); return;
 	}
 
+	ler_alunos(matriculas, nomes, linhas);
+
+	for (i = 0; i <= linhas; i++) {
+		if (strcmp(nomes[i], nome) >= 0) {
+			matricula = matriculas[i];
+			nome = nomes[i];
+			printf("Aluno %s(%d) tem média: %f", nome, matricula, media(matricula));
+		}
+	}
 }
