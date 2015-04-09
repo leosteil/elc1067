a44a9e49eeb9c8ff05fe0fe54eec7b1cb8813685
leosteil <leosteil@hotmail.com>
2015-04-09 11:42:38 -0300

    Aparece o console, falta fazer toda parte lógica do jogo 09/04

--- T4/principal.c
+++ T4/principal.c
@@ -27,42 +27,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> 
 
 #include "memo.h"
 #include "jogo.h"
 #include "vetor.h"
 
-int 
-main(int argc, char **argv)
-{
-	jogo		solit;
-	solit = jogo_cria();
-
-	pilha_insere_carta(jogo_monte(solit), carta_cria(AS, OUROS));
-	pilha_insere_carta(jogo_monte(solit), carta_cria(REI, PAUS));
-	pilha_insere_carta(jogo_monte(solit), carta_cria(DAMA, COPAS));
-	pilha_insere_carta(jogo_monte(solit), carta_cria(2, PAUS));
-	pilha_insere_carta(jogo_monte(solit), carta_cria(10, ESPADAS));
-
-	/*vetor_t* cartas = vetor_cria();*/
-	/*vetor_t* fora_ordem = vetor_cria();*/
+void inicia_jogo(jogo solit){
+	vetor_t* cartas = vetor_cria();
+	vetor_t* fora_ordem = vetor_cria();
+	srand(time(NULL));
 	int i,j;
+	carta c;
 	
-	/*for(i=0; i<4; i++){	// cria as cartas 
+	for(i=0; i<4; i++){	// cria as cartas 
 		for(j=1; j<=13;j++){
-			vetor_insere_carta(cartas, (i*13)+j-1, carta_cria(i,j));
+			vetor_insere_carta(cartas, (i*13)+j-1, carta_cria(j,i));
 		}
 	}
 
-	for (i=0; i<vetor_numelem(cartas);i++){ // embaralhamento das cartas s
-		j =  ((int)rand())% vetor_numelem(cartas);
-		carta c = vetor_remove_carta(cartas,j);
-		vetor_insere_carta(fora_ordem,i,c);	
+	for (i=0; i<52;i++){ // embaralhamento das cartas
+		do{
+			j=((int) rand()) % vetor_numelem(cartas);
+			c = vetor_remove_carta(cartas,j);
+		}while(c == NULL);
+		vetor_insere_carta(fora_ordem, i, c);
 	}	
 	
 	for(i=0; i<7; i++){ // pilhas
-		carta c;
-
+		
 		for(j=0;j<=i; j++){ // cartas
 			c = vetor_remove_carta(fora_ordem,0);
 			pilha_insere_carta(jogo_pilha(solit,i),c);		
@@ -70,13 +63,25 @@ main(int argc, char **argv)
 		carta_abre(c); // esta no topo
 	}
 	
-	for(i=0; i<vetor_numelem(fora_ordem); i++){
+	for(i=0; i<52-28; i++){
 		carta c = vetor_remove_carta(fora_ordem,0);
 		pilha_insere_carta(jogo_monte(solit),c);	
 	}
-	
-	vetor_destroi(cartas);
-	vetor_destroi(fora_ordem);*/
+}
+
+int 
+main(int argc, char **argv)
+{
+	jogo		solit;
+	solit = jogo_cria();
+
+	inicia_jogo(solit);
+
+	/*pilha_insere_carta(jogo_monte(solit), carta_cria(AS, OUROS));
+	pilha_insere_carta(jogo_monte(solit), carta_cria(REI, PAUS));
+	pilha_insere_carta(jogo_monte(solit), carta_cria(DAMA, COPAS));
+	pilha_insere_carta(jogo_monte(solit), carta_cria(2, PAUS));
+	pilha_insere_carta(jogo_monte(solit), carta_cria(10, ESPADAS));*/
 
 	jogo_desenha(solit);
 	while (!pilha_vazia(jogo_monte(solit))) {
